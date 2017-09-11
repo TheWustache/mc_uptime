3ffc66406089ee79565a1d4f23b63eb774fbc52f
TheWustache <leonard.geier@hotmial.com>
2017-09-11 13:39:08 +0200

    added base.html template for jinja, adjusted login.html accordingly, added style.css

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,6 @@
-Server for coordinating up-time of other servers with other people.
+Server for coordinating up-time of further server-software with other people.
+
+## Setup
+* `virtualenv venv`
+* `pip install Flask`
+* `export FLASK_APP=hello.py`
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, redirect, url_for, current_app, request
+from flask import Flask, session, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,7 +13,7 @@ def login():
     if request.method == 'POST':
         session['username'] = 'set'
         return redirect(url_for('index'))
-    return current_app.send_static_file('login.html')
+    return render_template('login.html')
 
-# set the secret key.  keep this really secret:
+# set the secret key
 app.secret_key = '\xa5\xb8n0K~\xdb\x1d\xb2\xb0v\x0f\x03\xc5-\x8c\x94\xeeBI\xb4q\xbc\xac'
